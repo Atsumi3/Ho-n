21caebf6de65e1068c15c01a38df971a9fe9dcb8
atsumi <atsumi3041@yahoo.co.jp>
2016-10-23 04:20:42 +0900

    fix layout

--- app/src/main/java/info/nukoneko/android/ho_n/controller/common/view/NKSquareImageView.java
+++ app/src/main/java/info/nukoneko/android/ho_n/controller/common/view/NKSquareImageView.java
@@ -1,6 +1,7 @@
 package info.nukoneko.android.ho_n.controller.common.view;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
